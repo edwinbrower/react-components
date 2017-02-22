e757aff80cd0eccae45c24ebbae9ffdea23aa7a9
Edwin Brower <edwin.brower@gmail.com>
2017-02-22 13:13:58 -0800

    Up to classes

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li>{this.props.item}</li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
+  <div>
+    <h2>Edwin's Basik Grocery List</h2>
+    <ul>
+      {props.groceryItems.map( (item) =>
+        <GroceryListItem item = {item} />
+      )}
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList groceryItems = {['Acai', 'Kale']} />, document.getElementById('app'));
+
